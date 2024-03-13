19426e6a609362cd09a1879b5829d5c555d5faaf
Zed pagodeiro <julio57.pa@hotmail.com>
2024-03-12 21:29:36 -0300

    Primeira estrutura do projeto eclipse

---
+++ src/main/java/model/Aluno.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Aluno {
+	private UUID id;
+	private String nome;
+}
---
+++ src/main/java/model/Teste.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		System.out.println("teste");
+
+	}
+
+}
